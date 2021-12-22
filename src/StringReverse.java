6f3cc4ee8da91cba7766335fa0ed4e6e10ff729f
ragor114 <69634980+ragor114@users.noreply.github.com>
2021-12-22 16:40:28 +0100

    added solution

--- src/StringReverse.java
+++ src/StringReverse.java
@@ -2,7 +2,19 @@ public class StringReverse {
 
     /* Schreiben Sie eine Methode, die einen String als Parameter übergeben bekommt und in umgekehrter Reihenfolge zurückgibt.*/
     public static String kataStringReversed(String str) {
-        return "";
+        if(str == null || str.length() == 0) {
+            return "";
+        }
+
+        String reversed = "";
+
+        // Die for-Schleife zählt vom Index des letzten Zeichens bis 0 runter.
+        for (int i = str.length()-1; i >= 0; i--) {
+            // Die Zeichen werden in umgkehrter Reihenfolg an reversed konkateniert.
+            reversed += str.charAt(i);
+        }
+
+        return reversed;
     }
 
     public static void main(String[] args) {
